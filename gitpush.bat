6e91a993734290565b360a8bc765937a151b5b45
rweilwww <github@shaw-weil.com>
2025-10-19 10:36:14 -0400

    from inDesign 2025-10-19 14:35

--- gitpush.bat
+++ gitpush.bat
@@ -1,14 +1,9 @@
 
 cd /inetpub/wwwroot/w/oilcity
-git init
-            git config --global --add safe.directory C:/inetpub/wwwroot/w/oilcity
-   git remote add origin https://github.com/rweilwww/web-oilcity.git
-   git remote -v
 
-        
 
 git add .
-git commit -m "from inDesign 2025-10-19 11:08" --no-edit
+git commit -m "from inDesign 2025-10-19 14:35" --no-edit
 git push --force --set-upstream origin main
 
 rem wait for one minute to allow git hub to get its act together
