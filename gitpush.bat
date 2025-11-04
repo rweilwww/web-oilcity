db054ce14f173c9a2acb46261acca93d0069f8b7
rweilwww <github@shaw-weil.com>
2025-11-03 19:21:49 -0500

    from inDesign 2025-11-04 00:20

--- gitpush.bat
+++ gitpush.bat
@@ -3,11 +3,10 @@ cd /inetpub/wwwroot/w/oilcity
 
 
 git add .
-git commit -m "from inDesign 2025-10-23 12:38" --no-edit
+git commit -m "from inDesign 2025-11-04 00:20" --no-edit
 git push --force --set-upstream origin main
 
 rem wait for one minute to allow git hub to get its act together
 timeout 60
-rem go fo it!!
 timeout 5
 exit
